6ada3df8382afc2fee4c4f3efca95db6f1b8c7b6
Elsabet Gebre <89805409+ElsabetG@users.noreply.github.com>
2022-04-19 22:15:24 +0300

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,99 +1,47 @@
 #include "main.h"
 /**
- * print_format - format controller
- * @format: the base string
- * @valist : hold the argument passed
- * Return: total size of the argument with the total size of the base string
- */
-int print_format(const char *format, va_list valist)
-{
-	unsigned int count = 0;
-	int result;
-	int i = 0;
-
-	for (i = 0; format[i]; i++)
-	{
-		if (format[i] == '%')
-		{
-			result = formatchecker(format, valist, &i);
-			if (result == -1)
-			{
-				return (-1);
-			}
-		count += result;
-		continue;
-		}
-	print_out(format[i]);
-	count++;
-	}
-	return (count);
-}
-
-/**
- * formatchecker - checks the format and print the character
- * @str: the base string
- * @valist: number of arguments passed
- * @j: address of %
- * Return: total number of printed charcter inside the argument
- */
-int formatchecker(const char *str, va_list valist, int *j)
-{
-	int i;
-	int p;
-	int formats;
-
-	Data checker[] = {{'c', print_char},
-			  {'s', print_string},
-			  {'d', print_int},
-			  {'i', print_int},
-			  {'b', print_binary},
-			  {'u', print_unsigned},
-			  {'o', print_octal},
-			  {'x', print_hex},
-			  {'X', print_hex_big},
-			  {'S', print_bigS},
-			  {'p', print_address},
-			  {'R', print_rot13}};
-	*j = *j + 1;
-	if (str[*j] == '\0')
-	{
-		return (-1);
-	}
-	if (str[*j] == '%')
-	{
-		print_out('%');
-		return (1);
-	}
-	formats = sizeof(checker) / sizeof(checker[0]);
-	for (i = 0; i < formats; i++)
-	{
-		if (str[*j] == checker[i].l)
-		{
-			p = checker[i].ptr(valist);
-			return (p);
-		}
-	}
-	print_out('%'), print_out(str[*j]);
-	return (2);
-}
-
-/**
- * _printf - prints anything
- * @format: list of argument type passed
- * Return: number of character printed
- */
+* _printf - main function to print in console
+* @format: array to print and check type
+* Return: count of character printed
+**/
 int _printf(const char *format, ...)
 {
-	va_list ag;
-	int f;
+	int count = -1;
 
-	if (format == NULL)
+	if (format != NULL)
 	{
-		return (-1);
+		int i;
+		va_list ar_list;
+		int (*o)(va_list);
+
+		va_start(ar_list, format);
+
+		if (format[0] == '%' && format[1] == '\0')
+			return (-1);
+
+		count = 0;
+
+		for (i = 0; format[i] != '\0'; i++)
+		{
+			if (format[i] == '%')
+			{
+				if (format[i + 1] == '%')
+				{
+					count += _putchar(format[i]);
+					i++;
+				}
+				else if (format[i + 1] != '\0')
+				{
+					o = get_func(format[i + 1]);
+					count += (o ? o(ar_list) : _putchar(format[i]) + _putchar(format[i + 1]));
+					i++;
+				}
+			}
+			else
+				count += _putchar(format[i]);
+		}
+		va_end(ar_list);
 	}
-	va_start(ag, format);
-	f = print_format(format, ag);
-	print_out(-1);
-	va_end(ag);
-	return (f);
+
+	return (count);
 }
